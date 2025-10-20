2ec278403b515b3e51fdd589a664556e195e1cc6
AsifAhmedTanjid <asifahmedtanjid@gmail.com>
2025-10-20 15:02:24 +0600

    user defined type (tuple)

---
+++ user-defined-type/tuple-type.js
@@ -0,0 +1,6 @@
+//mixed data -key ,value
+var user;
+user = [101, "asif"];
+user.push(102, "ssg");
+console.log(user);
+console.log(user[0]);
---
+++ user-defined-type/tuple-type.ts
@@ -0,0 +1,7 @@
+//mixed data -key ,value
+let user : [number,string]
+user =[101,"asif"]
+user.push(102,"ssg")
+
+console.log(user);
+console.log(user[0]);
