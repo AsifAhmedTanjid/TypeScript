1ff890193d55f412e6a299951102db1f993f934c
AsifAhmedTanjid <asifahmedtanjid@gmail.com>
2025-10-20 15:51:53 +0600

    any type

--- user-defined-type/object.js
+++ user-defined-type/object.js
@@ -1,29 +1,29 @@
-var user1;
-user1 = {
-    userName: "ASIF",
-    userId: 101
-};
-console.log(user1);
-var user2;
-user2 = {
-    name: "111",
-    id: 1
-};
-var users;
-users = [];
-var user101;
-user101 = {
-    name: "asif",
-    id: 101,
-};
-var user102;
-user102 = {
-    name: "Rafid",
-    id: 102,
-};
-users.push(user101);
-users.push(user102);
-for (var _i = 0, users_1 = users; _i < users_1.length; _i++) {
-    var user = users_1[_i];
-    console.log(user);
-}
+// var user1;
+// user1 = {
+//     userName: "ASIF",
+//     userId: 101
+// };
+// console.log(user1);
+// var user2;
+// user2 = {
+//     name: "111",
+//     id: 1
+// };
+// var users;
+// users = [];
+// var user101;
+// user101 = {
+//     name: "asif",
+//     id: 101,
+// };
+// var user102;
+// user102 = {
+//     name: "Rafid",
+//     id: 102,
+// };
+// users.push(user101);
+// users.push(user102);
+// for (var _i = 0, users_1 = users; _i < users_1.length; _i++) {
+//     var user = users_1[_i];
+//     console.log(user);
+// }
