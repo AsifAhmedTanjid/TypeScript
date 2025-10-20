5333262352d6f58ccc3ba96d8972fc75f1251c60
AsifAhmedTanjid <asifahmedtanjid@gmail.com>
2025-10-20 14:48:40 +0600

    user defined type (array)

---
+++ user-defined-type/user-define-type.js
@@ -0,0 +1,8 @@
+// let userNames:string[]
+var userNames;
+userNames = ["asif", "rahul", "nafid", "rahimin", "hello"];
+userNames.sort();
+console.log(userNames);
+var multipleType;
+multipleType = [1, "name"];
+console.log(multipleType);
---
+++ user-defined-type/user-define-type.ts
@@ -0,0 +1,9 @@
+// let userNames:string[]
+let userNames:Array<string>
+userNames=["asif","rahul","nafid","rahimin","hello"]
+userNames.sort()
+console.log(userNames);
+let multipleType : (string| number) [];
+multipleType =[1,"name"];
+console.log(multipleType);
+
